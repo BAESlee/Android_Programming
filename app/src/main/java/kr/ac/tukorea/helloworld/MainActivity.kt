ccd44bb70bd4afade1e11959854989005df3c34b
John Doe <lees13579@tukorea.ac.kr>
2022-07-18 12:59:59 +0900

    test1

--- app/src/main/java/kr/ac/tukorea/helloworld/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/helloworld/MainActivity.kt
@@ -10,10 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1 = findViewById<Button>(R.id.button1)
 
-        button1.setOnClickListener {
-            Toast.makeText(applicationContext,"버튼 누름",Toast.LENGTH_SHORT).show()
-        }
     }
 }
