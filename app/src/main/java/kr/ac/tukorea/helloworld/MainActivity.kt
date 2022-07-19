e4e257a9a17e52e466a12358a101a80579c1a4c0
John Doe <lees13579@tukorea.ac.kr>
2022-07-19 12:15:32 +0900

    실습 5-1 (xml 없음)

--- app/src/main/java/kr/ac/tukorea/helloworld/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/helloworld/MainActivity.kt
@@ -1,17 +1,43 @@
 package kr.ac.tukorea.helloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import org.w3c.dom.Text
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title="애완동물 사진 보기"
+        // setContentView(R.layout.activity_main)
+        title="실습 5-1"
 
+        var params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT)
 
+        var baseLayout = LinearLayout(this)
+        baseLayout.orientation = LinearLayout.VERTICAL
+        baseLayout.setBackgroundColor(Color.rgb(0,255,0))
+        setContentView(baseLayout,params)
+
+        var btn = Button(this)
+        btn.text= "버튼입니다"
+        btn.setBackgroundColor(Color.LTGRAY)
+        baseLayout.addView(btn)
+
+        var red = 0
+        var green = 0
+        var blue = 0
+
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext,"코드로 생성한 버튼",Toast.LENGTH_SHORT).show()
+            red = Random().nextInt(256)
+            green = Random().nextInt(256)
+            blue = Random().nextInt(256)
+            baseLayout.setBackgroundColor(Color.rgb(red,green,blue))
+        }
     }
 }
